0dc94b83b811a465dce2c2c936ac2b7d2fa4b7fe
Jameskmonger <jameskmonger@hotmail.co.uk>
2016-04-26 19:09:24 +0100

    Create noodle endpoint

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,20 @@
 import * as express from 'express';
 
 let app = express();
+
+app.get('/noodles/', (req, res) => {
+    res.send([
+        {
+            id: 0,
+            name: 'Prawn Chilli Noodle',
+            description: 'spicy yum yum'
+        },
+        {
+            id: 1,
+            name: 'Shredded Chicken Noodle',
+            description: 'Nicer than fried chicken'
+        }
+    ]);
+});
+
+app.listen(3000);
