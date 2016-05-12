6cb404fd9fa5bf5c45d50cbace255db08512fe1b
Jameskmonger <jameskmonger@hotmail.co.uk>
2016-05-12 19:20:29 +0100

    Create example reviews

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@
 
 import * as express from 'express';
 import { ReviewSerializer } from "./serializers/review-serializer";
+import { ReviewModel } from "./models/review-model";
 
 let app = express();
 let reviewSerializer = new ReviewSerializer(); // get some nice IoC here
@@ -11,6 +12,11 @@ app.use((req, res, next) => {
 
     next();
 });
+
+let reviews: Array<ReviewModel> = [];
+reviews.push(new ReviewModel("Great noodles, top class.", "These really have to be among some of the best noodles I've ever tasted. Their noodliness was as noodly as it gets.", 100.00));
+reviews.push(new ReviewModel("Not a fan", "noodles were too spicy for my tongue", 0.00));
+reviews.push(new ReviewModel("They were alright, I suppose...", "They weren't the best, but they weren't the worst. Food's food, I guess.", 50.00));
 
 app.get('/noodles/', (req, res) => {
     res.send([
