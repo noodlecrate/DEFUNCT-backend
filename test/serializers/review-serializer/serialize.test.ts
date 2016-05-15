5667bfbfc441f806b9f89384eca29dfb3a9f4840
James Monger <jameskmonger@hotmail.co.uk>
2016-05-15 23:31:51 +0100

    Test id serialization

--- test/serializers/review-serializer/serialize.test.ts
+++ test/serializers/review-serializer/serialize.test.ts
@@ -1,5 +1,20 @@
 /// <reference path="../../../typings/main.d.ts" />
 
 import { ReviewSerializer } from '../../../src/serializers/review-serializer';
+import { ReviewModel } from "../../../src/models/review-model";
 
 const test = require('modunit');
+
+test('id is serialized correctly', (t, inputId) => {
+
+    let serializer = new ReviewSerializer();
+    let model = new ReviewModel(inputId, 'test title', 'test body', 100.00);
+    let json = serializer.serialize(model);
+
+    t.assert.equal(inputId, json.id);
+
+}, [
+    [ 5 ],
+    [ 20 ],
+    [ 256 ]
+]);
