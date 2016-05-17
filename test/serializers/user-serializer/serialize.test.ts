bbf3811fd4f7faacb9108427c38a8b825edb9d8e
Jameskmonger <jameskmonger@hotmail.co.uk>
2016-05-17 18:37:40 +0100

    Test serialize id

--- test/serializers/user-serializer/serialize.test.ts
+++ test/serializers/user-serializer/serialize.test.ts
@@ -2,3 +2,19 @@
 
 import { UserSerializer } from '../../../src/serializers/user-serializer';
 import { UserModel } from "../../../src/models/user-model";
+
+const test = require('modunit');
+
+test('id is serialized correctly', (t, inputId) => {
+
+    let serializer = new UserSerializer();
+    let model = new UserModel(inputId, 'joe.bloggs', 'Joe', 'Bloggs');
+    let json = serializer.serialize(model);
+
+    t.assert.equal(inputId, json.id);
+
+}, [
+    [ 5 ],
+    [ 20 ],
+    [ 256 ]
+]);
