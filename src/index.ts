58c39c9300a551218db9161ab1ce048be45680ae
Jameskmonger <jameskmonger@hotmail.co.uk>
2016-05-17 18:25:37 +0100

    add images

--- src/index.ts
+++ src/index.ts
@@ -14,9 +14,9 @@ app.use((req, res, next) => {
 });
 
 let reviews: Array<ReviewModel> = [];
-reviews[1] = new ReviewModel(1, "Great noodles, top class.", "These really have to be among some of the best noodles I've ever tasted. Their noodliness was as noodly as it gets.", 100.00);
-reviews[2] = new ReviewModel(2, "Not a fan", "noodles were too spicy for my tongue", 0.00);
-reviews[3] = new ReviewModel(3, "They were alright, I suppose...", "They weren't the best, but they weren't the worst. Food's food, I guess.", 50.00);
+reviews[1] = new ReviewModel(1, "Great noodles, top class.", "These really have to be among some of the best noodles I've ever tasted. Their noodliness was as noodly as it gets.", 100.00, 'https://i.ytimg.com/vi/XutaTTNihe0/hqdefault.jpg');
+reviews[2] = new ReviewModel(2, "Not a fan", "noodles were too spicy for my tongue", 0.00, 'http://i.dailymail.co.uk/i/pix/2014/09/03/article-2740204-1C5E561F00000578-366_634x626.jpg');
+reviews[3] = new ReviewModel(3, "They were alright, I suppose...", "They weren't the best, but they weren't the worst. Food's food, I guess.", 50.00, 'http://i798.photobucket.com/albums/yy262/expressionofmyemotions/tumblr_lrud0coRjU1qhsw0o.gif');
 
 app.get('/reviews/', (req, res) => {
     let serialized: Array<any> = [];
