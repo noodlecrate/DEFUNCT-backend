8555b75a1cc2ebb154b07051ea8f31836864d07c
James Monger <jameskmonger@hotmail.co.uk>
2016-05-22 23:31:20 +0100

    Fix noodle constructors

--- src/repositories/noodle-repository.ts
+++ src/repositories/noodle-repository.ts
@@ -2,64 +2,50 @@ import { NoodleModel } from "../models/noodle-model";
 import { CountryRepository } from "./country-repository";
 import { BrandRepository } from "./brand-repository";
 
-let noodles =  [
-    new NoodleModel(
-            1,
-            'Prawn Chilli Noodle',
-            'spicy yum yum',
-            'http://i.imgur.com/RD2djUw.jpg?1')
-    ,
-    new NoodleModel(
-            2,
-            'Shredded Chicken Noodle',
-            'Nicer than fried chicken',
-            'http://i.imgur.com/WuJVusY.jpg?1')
-    ,
-    new NoodleModel(
-            3,
-            'Imaginary Noodle',
-            'who knows...',
-            '')
-];
-
 export class NoodleRepository {
 
   private _countryRepository = new CountryRepository();
   private _brandRepository = new BrandRepository();
 
+  private _noodles: Array<NoodleModel>;
+
+  constructor() {
+      let countries = this._countryRepository.getAll();
+      let brands = this._brandRepository.getAll();
+
+      this._noodles = [
+          new NoodleModel(
+              1,
+              'Prawn Chilli Noodle',
+              'spicy yum yum',
+              'http://i.imgur.com/RD2djUw.jpg?1',
+              countries[0],
+              brands[0]
+          ),
+          new NoodleModel(
+              2,
+              'Shredded Chicken Noodle',
+              'Nicer than fried chicken',
+              'http://i.imgur.com/WuJVusY.jpg?1',
+              countries[1],
+              brands[1]
+          ),
+          new NoodleModel(
+              3,
+              'Imaginary Noodle',
+              'who knows...',
+              '',
+              countries[2],
+              brands[2]
+          )
+      ];
+  }
+
   getAll(): Array<NoodleModel> {
-    let noodles =  [
-        new NoodleModel(
-                1,
-                'Prawn Chilli Noodle',
-                'spicy yum yum',
-                'http://i.imgur.com/RD2djUw.jpg?1')
-        ,
-        new NoodleModel(
-                2,
-                'Shredded Chicken Noodle',
-                'Nicer than fried chicken',
-                'http://i.imgur.com/WuJVusY.jpg?1')
-        ,
-        new NoodleModel(
-                3,
-                'Imaginary Noodle',
-                'who knows...',
-                '')
-    ];
-
-    let countries = this._countryRepository.getAll();
-    let brands = this._brandRepository.getAll();
-
-    noodles.forEach(noodle => {
-      (<any>noodle).countryOfOrigin = countries[noodle.id - 1];
-      (<any>noodle).brand = brands[noodle.id - 1];
-    })
-
-    return noodles;
+      return this._noodles;
   }
 
   public insert(noodle: NoodleModel) {
-    noodles[noodles.length + 1] = noodle;
+    this._noodles[this._noodles.length + 1] = noodle;
   }
 }
