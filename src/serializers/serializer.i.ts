5cf90622c9a866b0729db01c0771c4e9fe80d42b
James Monger <jameskmonger@hotmail.co.uk>
2016-05-24 00:00:41 +0100

    Put TModel constraint on ISerializer

--- src/serializers/serializer.i.ts
+++ src/serializers/serializer.i.ts
@@ -1,4 +1,6 @@
-export interface ISerializer<TModel> {
+import { IModel } from "../models/model.i";
+
+export interface ISerializer<TModel extends IModel> {
 
     serialize(model: TModel): any;
     deserialize(json: any): TModel;
